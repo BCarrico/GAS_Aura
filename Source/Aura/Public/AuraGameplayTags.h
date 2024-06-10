132b75b307ee528224b3e68060e67e62f1298141
bradley carrico <BCarrico89@gmail.com>
2024-06-09 18:10:53 -0700

    Input Config Data Asset

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -32,6 +32,13 @@ static const FAuraGameplayTags& Get(){ return GameplayTags;}
  FGameplayTag Attributes_Secondary_ManaRegeneration;
  FGameplayTag Attributes_Secondary_MaxMana;
  FGameplayTag Attributes_Secondary_MaxHealth;
+
+ FGameplayTag InputTag_LMB;
+ FGameplayTag InputTag_RMB;
+ FGameplayTag InputTag_1;
+ FGameplayTag InputTag_2;
+ FGameplayTag InputTag_3;
+ FGameplayTag InputTag_4;
 protected:
 
 private:
