65715b9f6efd79a7205f6e0967302c4dc784aba7
bradley carrico <BCarrico89@gmail.com>
2024-06-11 15:33:31 -0700

    Receiving Target Data

--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -3,6 +3,7 @@
 
 #include "AuraAssetManager.h"
 #include "AuraGameplayTags.h"
+#include "AbilitySystemGlobals.h"
 
 UAuraAssetManager& UAuraAssetManager::Get()
 {
@@ -15,5 +16,7 @@ void UAuraAssetManager::StartInitialLoading()
 {
 	Super::StartInitialLoading();
 	FAuraGameplayTags::InitializeNativeGameplayTags();
-	
+
+	// Required to use Target Data!
+	UAbilitySystemGlobals::Get().InitGlobalData();
 }
