940ab0053046659b6eb736834ae0b43cacd21263
bradley carrico <BCarrico89@gmail.com>
2024-06-18 09:08:26 -0700

    Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+// Copyright KonaBear
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
