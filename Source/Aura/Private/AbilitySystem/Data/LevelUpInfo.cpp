b47d5613cbfc311cbbea5d566cded5872d46edd8
bradley carrico <BCarrico89@gmail.com>
2024-06-27 16:18:32 -0700

    Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,25 @@
+// Copyright KonaBear
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// Level 0 in the Data Asset is a placeholder
+		// LevelUpInformation[1] = Level 1 Information.  LevelUpInformation[2] = Level 1 Information
+		if (LevelUpInformation.Num()-1 <= Level) return Level;
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
