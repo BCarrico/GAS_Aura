6c73224203808a56b54a3760ea5414282458de92
bradley carrico <BCarrico89@gmail.com>
2024-07-15 15:10:45 -0700

    Aura Fire Ball

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -36,7 +36,7 @@ protected:
 	virtual void Destroyed() override;
 	void OnHit();
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 private:
 	UPROPERTY(EditDefaultsOnly)
 	float LifeSpan = 15.f;
